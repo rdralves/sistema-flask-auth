602aa3fbf46cae108cff16e209fbf875d319e4e0
rdralves <rdr.alves@gmail.com>
2025-04-10 14:23:08 -0300

    Etapa 1, estrutura inicial com hello word funcionando

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    
+    
+    @app.route('/')
+    def hello():
+        return 'Hello, Word!'
+    
+    return app 
