6dae2817dbaacee21ec5a2202c1bce52f325a674
rdralves <rdr.alves@gmail.com>
2025-04-10 19:34:58 -0300

    Etapa formulario de login criado

--- app/models/user.py
+++ app/models/user.py
@@ -5,6 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    # Adicionamos o campo senha
+    password = db.Column(db.String(128), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,8 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from app.models.user import User
+from app import db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    mensagem = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(
+            username=username, password=password).first()
+
+        if user:
+            mensagem = f"Bem-vindo, {user.username}!"
+        else:
+            mensagem = "Usuário ou senha inválidos."
+
+    return render_template('login.html', mensagem=mensagem)
