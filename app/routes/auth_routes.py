3ac0d9963d6266f655d3bf8b14f51db341c58271
rdralves <rdr.alves@gmail.com>
2025-04-14 10:03:10 -0300

    finalização da parte de cadastro, login/logout, session

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from app.models.user import User
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -12,12 +13,43 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = User.query.filter_by(
-            username=username, password=password).first()
+        user = User.query.filter_by(username=username).first()
 
-        if user:
-            mensagem = f"Bem-vindo, {user.username}!"
+        if user and check_password_hash(user.password, password):
+            session['usuario_id'] = user.id
+            session['usuario_nome'] = user.username
+            return redirect(url_for('main.dashboard'))
         else:
             mensagem = "Usuário ou senha inválidos."
 
     return render_template('login.html', mensagem=mensagem)
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()  # limpa a sessão
+    return redirect(url_for('auth.login'))
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    mensagem = None
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # verificar se o usuário ou e-mail já existem
+        if User.query.filter_by(username=username).first():
+            mensagem = "Usuário já cadastrado."
+        elif User.query.filter_by(email=email).first():
+            mensagem = "Email já cadastrado."
+        else:
+            hashed_pw = generate_password_hash(password)
+            novo_user = User(username=username, email=email,
+                             password=hashed_pw)
+            db.session.add(novo_user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+
+    return render_template('register.html', mensagem=mensagem)
--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session,redirect,url_for
 
 bp = Blueprint('main', __name__)
 
@@ -9,3 +9,12 @@ def index():
 @bp.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+
+@bp.route('/dashboard')
+def dashboard():
+    if 'usuario_id' not in session:
+        return redirect(url_for('auth.login'))
+
+    nome = session.get('usuario_nome')
+    return render_template('dashboard.html', nome=nome)
